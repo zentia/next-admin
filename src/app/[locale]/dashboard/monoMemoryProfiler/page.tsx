c1d3992da3a9bea54e61b1905663306f7ce86904
zentiali <zentiali@tencent.com>
2024-06-20 22:53:39 +0800

    os

--- src/app/[locale]/dashboard/monoMemoryProfiler/page.tsx
+++ src/app/[locale]/dashboard/monoMemoryProfiler/page.tsx
@@ -1,13 +1,44 @@
+'use client'
 import Layout from '@/components/Layout';
-
+import React from 'react';
+import dynamic from "next/dynamic";
 import styles from './index.module.less';
 
+const Heatmap = dynamic(
+    ()=>import('@ant-design/charts').then((mod)=>mod.Heatmap),
+    {
+        ssr:false
+    }
+)
+
+import boardList from './board';
+
 export default function Dashboard(){
+
+    const config = {
+        height: 300,
+        autoFit: false,
+        boardList,
+        xField: (d: { date: string | number | Date; }) => new Date(d.date).getUTCDate(),
+        yField: (d: { date: string | number | Date; }) => new Date(d.date).getUTCMonth(),
+        colorField: 'temp_max',
+        legend: {},
+        mark: 'cell',
+        style: { inset: 0.5 },
+        tooltip: {
+            title: 'date',
+            field: 'temp_max',
+            valueFormatter: '~s',
+            pointerEvents: 'none'
+        },
+    };
+
     return (
         <Layout curActive='/dashboard/monoMemoryProfiler'>
-            <main className={styles.monotorWrap}>
-                
+            <main className={styles.monitorWrap}>
+                <Heatmap {...config} />
             </main>
         </Layout>
     );
+
 }
